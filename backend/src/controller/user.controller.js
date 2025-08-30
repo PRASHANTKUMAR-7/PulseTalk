22b203cab2ab6b179d35e91f5727e09ddc587d8b
PRASHANTKUMAR-7 <prashantkumar74806@gmail.com>
2025-08-31 04:02:36 +0530

    adding all route to check each possibilities of friend request

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.js";
+import FriendRequest from "../models/FriendRequest.js";
 
 //route for recomending user
 export async function getRecommendedUsers(req,res){
@@ -39,9 +40,40 @@ export async function getMyFriends(req,res){
 //route to send friends request 
 export async function sendFriendRequest(req,res){
     try {
-        
+        const myId=req.user.id;
+        const {id:recipientId}=req.param;
+
+        //prevent sending req to yourself
+        if(myId===recipientId){
+            return res.status(400).json({message: "You can't send friend request to yourself"});
+        }
+        const recipient= await User.findById(recipientId);
+        if(!recipient){
+            res.status(404).json({message: "Recipient not found"});
+        }
+//check user is already friend of current user or not 
+        if(recipient.friends.includes(myId)){
+            return res.status(400).json({message: "You are already friend with user"});
+        }
+
+//check is a friend request is already exits
+        const exitingRequest=await FriendRequest.findOne({
+            $or:[
+                {sender:myId,recipient:recipientId},
+                {sender: recipientId,recipient:myId},
+            ],
+        });
+        if(exitingRequest){
+            return res.status(400).json({message: "A friend request already exit"});
+        }
+        const friendRequest= await FriendRequest.create({
+            sender:myId,
+            recipient:recipientId,
+        });
+        res.status(201).json(friendRequest);
+
     } catch (error) {
-        
+        res.status(500).json({message: "Internet server Error"});
     }
 
 }
