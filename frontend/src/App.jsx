05c9280715caa8df1bce8d395215c3818fa47d8f
PRASHANTKUMAR-7 <prashantkumar74806@gmail.com>
2025-08-31 23:48:25 +0530

    adding different web page and react route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div className="h-screen text-5xl" data-theme="night">
-      hello my name is Prashant Kumar
+      <Route>
+        
+      </Route>
     </div>
   )
 }
---
+++ frontend/src/pages/SighUpPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SighUpPage = () => {
+  return (
+    <div>
+      SignUp Page
+    </div>
+  )
+}
+
+export default SighUpPage
