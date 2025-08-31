edd23f45a1d38e73ecc99314bb5b2a61274a9572
PRASHANTKUMAR-7 <prashantkumar74806@gmail.com>
2025-09-01 00:04:00 +0530

    removing error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import { Route, Routes } from 'react-router';
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SighUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import NotificationPage from "./pages/NotificationPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
 
 const App = () => {
   return (
-    <div className="h-screen text-5xl" data-theme="night">
-      <Route>
-        
-      </Route>
+    <div className="h-screen" data-theme="night">
+      <Routes>
+        <Route path="/" element= {<HomePage/>}></Route>
+        <Route path="/signup" element= {<SignUpPage/>}></Route>
+        <Route path="/login" element= {<LoginPage/>}></Route>
+        <Route path="/notification" element= {<NotificationPage/>}></Route>
+        <Route path="/call" element= {<CallPage/>}></Route>
+        <Route path="/chat" element= {<ChatPage/>}></Route>
+        <Route path="/onboarding" element= {<OnboardingPage/>}></Route>
+      </Routes>
     </div>
   )
 }
