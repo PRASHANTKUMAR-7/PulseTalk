aff205213ef6a3b6e8d677d091b6896fe8093cab
PRASHANTKUMAR-7 <prashantkumar74806@gmail.com>
2025-09-01 19:08:11 +0530

    installing lucide for glow css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router';
+import { Navigate, Route, Routes } from 'react-router';
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SighUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
@@ -12,30 +12,31 @@ import axios from "axios";
 import { axiosInstance } from './lib/axios.js';
 
 const App = () => {
-  // const time_to_start=22654;
+  // const time_to_start=2.33.00;
   //axios for frontend and backend relation
   //react querry or tanstack querry
-  const {data,isLoading,error} = useQuery({//this has a great feature that if it(useQuery/teanstack) fails then it try more then once to execute on the other hand usestate will try single time
-    queryKey: ["todo"],
-
+  const {data:authData,isLoading,error} = useQuery({//this has a great feature that if it(useQuery/teanstack) fails then it try more then once to execute on the other hand usestate will try single time
+    queryKey: ["authUser"],
     queryFn: async()=>{
       const res = await axiosInstance.get("r/auth/me");
       return res.data;
     },
     retry:false, //if you dont want to retry more than once
   });
-  console.log(data);
+  const authUser=authData?.user; //we use user b/c in auth.route we use user
+
+
   return (
     <div className="h-screen" data-theme="night">
       {/* <button onClick={()=>toast.error("Hello World")}>Create a Toast</button> //using react hot toast */}
       <Routes>
-        <Route path="/" element= {<HomePage/>}></Route>
-        <Route path="/signup" element= {<SignUpPage/>}></Route>
-        <Route path="/login" element= {<LoginPage/>}></Route>
-        <Route path="/notification" element= {<NotificationPage/>}></Route>
-        <Route path="/call" element= {<CallPage/>}></Route>
-        <Route path="/chat" element= {<ChatPage/>}></Route>
-        <Route path="/onboarding" element= {<OnboardingPage/>}></Route>
+        <Route path="/" element= {authUser? <HomePage/> :<Navigate to="/login"/>}/>  {/* if authUser is correct means authorised user then open homepage unless redirect to login page  */}
+        <Route path="/signup" element= {!authUser?<SignUpPage/> :<Navigate to="/"/>}/> 
+        <Route path="/login" element= {!authUser? <LoginPage/> :<Navigate to="/"/>}/> 
+        <Route path="/notification" element= {authUser? <NotificationPage/> :<Navigate to="/login"/>}/> 
+        <Route path="/call" element= {authUser? <CallPage/> :<Navigate to="/login"/>}/> 
+        <Route path="/chat" element= {authUser? <ChatPage/>:<Navigate to="/login"/>}/> 
+        <Route path="/onboarding" element= {authUser? <OnboardingPage/>:<Navigate to="/login"/>}/> 
       </Routes>
       <Toaster/>
     </div>
--- frontend/src/pages/SighUpPage.jsx
+++ frontend/src/pages/SighUpPage.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react"
 
 
 const SighUpPage = () => {
+
+  const [signupData , setSignupData]=useState({
+    fullName:"",
+    email:"",
+    password:"",
+  });
+  const handleSigup=(e)=>{
+    e.preventDefault();
+  }
+
+
   return (
-    <div>
-      SignUp Page
-    </div>
+    <div className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8" data-theme="forest">
+  <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+
+
+
+
+  </div>
+
+
+
+  
+</div>
   )
 }
 
