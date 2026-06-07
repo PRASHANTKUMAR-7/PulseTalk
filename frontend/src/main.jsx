d8ba79118ebb364fec17a923aa77310c43386ec9
PRASHANTKUMAR-7 <prashantkumar74806@gmail.com>
2026-06-07 20:27:50 +0530

    dir name error solved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import "stream-chat-react/dist/css/v2/index.css"; //make sure it above index.css
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import "stream-chat-react/dist/css/index.css"; //make sure it above index.css
+import App from './App.jsx';
+import './index.css';
 
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query';
 import { BrowserRouter } from "react-router";
 
 //creating querry client to user tenstack
 const queryClient = new QueryClient();
-import {
-  useQuery,
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
