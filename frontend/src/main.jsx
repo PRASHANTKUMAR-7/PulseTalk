7fc509842e7b771d7741f0ec0820be3d13eeee7d
PRASHANTKUMAR-7 <prashantkumar74806@gmail.com>
2026-06-07 20:44:50 +0530

    Fix stream-chat-react build issues

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import "stream-chat-react/dist/css/index.css"; //make sure it above index.css
+import "stream-chat-react/dist/css/v2/index.css"; //make sure it above index.css
 import App from './App.jsx';
 import './index.css';
 
